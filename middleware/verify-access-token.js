deec060a61e0621f11dd4294f185816bdbc363a8
wakhiwemathuthu <wakhiwemathuthu6@gmail.com>
2024-04-04 11:48:36 +0200

    modified:   Logs/access-log.txt 	modified:   Logs/error-log.txt 	modified:   api.rest 	modified:   data/users.json 	modified:   middleware/verify-access-token.js

--- middleware/verify-access-token.js
+++ middleware/verify-access-token.js
@@ -1,5 +1,11 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const usersDB = {
+  users: require("../data/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
 
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 
@@ -10,7 +16,12 @@ function verifyAccessToken(req, res, next) {
     return res.status(401).send("No auth token was sent");
   }
   const token = authHeaders.split(" ")[1];
-
+  const foundUser = usersDB.users.find((user) => user.accessToken === token);
+  if (!foundUser) {
+    return res
+      .status(404)
+      .json({ message: "No user with the provided access token was found." });
+  }
   jwt.verify(token, ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
       res.status(403).send("Invalid token");
