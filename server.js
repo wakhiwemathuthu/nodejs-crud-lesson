82705415727710518d96586a6bf984cc12183227
wakhiwemathuthu <wakhiwemathuthu6@gmail.com>
2024-04-06 21:20:00 +0200

    updated

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const postsRouter = require("./routes/posts");
 const verifyJWT = require("./middleware/verifyJWT");
 const errorLogger = require("./utils/errorLogger");
 
-const PORT = process.env.PORT||8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cookieParser());
